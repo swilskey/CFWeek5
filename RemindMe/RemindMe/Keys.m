4b36af28b6eeb135cf8e941e4944f625f44bcc19
swilskey <swilskey41@gmail.com>
2015-08-31 17:53:00 -0700

    Added Xcode Project

---
+++ RemindMe/RemindMe/Keys.m
@@ -0,0 +1,19 @@
+//
+//  Keys.m
+//  RemindMe
+//
+//  Created by Sam Wilskey on 8/31/15.
+//  Copyright (c) 2015 Wilskey Labs. All rights reserved.
+//
+
+#import "Keys.h"
+
+@implementation Keys
++ (NSString *)getApplicationKey {
+  return @"4XGcbNpXRiOeTIeXGNM3jwwQRpBmrXRuKOHNC2uA";
+}
+
++ (NSString *)getClientId {
+  return @"mJAIIh5ydH9Fc8DG9DS9FkVBgX3Ypd47MqlsPPyo";
+}
+@end
